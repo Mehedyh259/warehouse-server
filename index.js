0c1ca7bd1ec658d13a87704869b51b7b19a45463
Mehedyh259 <mehedyh259@gmail.com>
2022-05-01 13:29:33 +0600

    back to default cors

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
-app.use(
-    cors({
-        origin: "http://localhost:3000"
-    })
-);
+app.use(cors());
 app.use(express.json());
 
 
